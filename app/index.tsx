b094601baa76b9c3672476f694fc7365ff7e454e
fouratrachid <rachidfourat2@gmail.com>
2026-04-02 15:15:23 +0100

    refactor: Remove loading state handling from MainApp component

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,5 @@
-import { useAuthStore } from "@/stores/authStore";
 import { Redirect } from "expo-router";
-import { ActivityIndicator, View } from "react-native";
 
 export default function MainApp() {
-  const { isLoading } = useAuthStore();
-
-  if (isLoading) {
-    return (
-      <View className="flex-1 justify-center items-center bg-primary-50">
-        <ActivityIndicator size="large" color="#2865D1" />
-      </View>
-    );
-  }
-
   return <Redirect href="/(tabs)/home" />;
 }
